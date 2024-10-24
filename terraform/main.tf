a91f2b47877e3d8d974548ad3aae72f0f0cf0709
r3dact3d <redhatbrady@gmail.com>
2024-10-24 00:20:59 -0500

    add playbooks

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,20 +16,20 @@ terraform {
 
 terraform {
   backend "s3" {
-    bucket = "${var.backend_bucket}"
+    bucket = "tfstate-bucket-blinker19"
     key    = "terraform.tfstate"
-    region = "${var.aws_region}"
+    region = "us-east-2"
   }
 }
 
 provider "aws" {
-  region = "${var.aws_region}"
+  region = "us-east-2"
 }
 
 # Add key for ssh connection
 resource "aws_key_pair" "my_key" {
   key_name   = "my_key"
-  public_key = "${var.pub_key}"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC0YP818L8HTt+pKUU+XPD8dJ9kYDhtplUKaodICGcS63A6EgdGGaxh45DVz8JmTNbP3RHQw6XbfTjNGmOO56UaGxQOsc+ONZ8fFjd+qa+7hBo6tIlrdRkrgZgKNDhTh4HijDgaqpPLhXroUK2TE61CSCiJezVbwwXtXU43wQYoeR06E+Ji1lfLLb5b5pIuUKwTRwa+6u9zL7JrDznKq5YZxsmkX3PNI9gHQT+SnSqPOGctXhbMQX7JWZA60EFx8MZXe8O9QC3LMrgNv90CCR9qnyd7/WTtb+lk/7lTYbFfj2W0WsQZMc2tnvoNv8azeCQcSHs6U2nsKd7lxXmmD0OFtXxSqI/O1628Q71sFjPIvET04I9ENHaAWwaI3s98I3Lt8Z5NLNqHrxwhmrFT5mTdn91Fzq4Ax7UKqcVG8Rtkzg7HnXL6nLIQs/cdRprysJIGC0aEpoHSN1OTqMcJkP4ySv5aYgT/G68Uau5JkBS8tKbeKNw+KE4Aq6tUJ+3etYc= brthomps@brthomps-thinkpadx1carbongen9.remote.csb"
 }
 
 resource "aws_vpc" "php_vpc" {
@@ -67,7 +67,7 @@ resource "aws_route_table" "php_pub_igw" {
 }
 
 resource "aws_subnet" "php_subnet" {
-  availability_zone       = "${var.aws_az}"
+  availability_zone       = "us-east-2a"
   cidr_block              = "10.1.0.0/24"
   map_public_ip_on_launch = "true"
   vpc_id                  = aws_vpc.php_vpc.id
@@ -136,13 +136,13 @@ data "aws_ami" "rhel" {
 }
 
 resource "aws_instance" "php_instance" {
-  instance_type               = "${var.aws_insance_type}"
+  instance_type               = "t2.xlarge"
   vpc_security_group_ids      = [aws_security_group.php_security_group.id]
   associate_public_ip_address = true
   key_name        = aws_key_pair.my_key.key_name
   user_data                   = file("user_data.txt")
   ami                         = data.aws_ami.rhel.id
-  availability_zone           = "${var.aws_az}"
+  availability_zone           = "us-east-2a"
   subnet_id                   = aws_subnet.php_subnet.id
 
   tags = {
@@ -161,5 +161,3 @@ resource "ansible_host" "php_instance" {
     ansible_python_interpreter   = "/usr/bin/python3",
   }
 }
-
-
